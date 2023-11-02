29323630c31194530ab79a81f6c4b3677d2dde71
mikl123 <bulesnmisha3@gmil.com>
2023-11-02 23:24:21 +0200

    Commented all things connected to Docker and DataBase!!!!!

--- src/main/java/com/example/demo/Repository/FlowerRepository.java
+++ src/main/java/com/example/demo/Repository/FlowerRepository.java
@@ -1,11 +1,11 @@
-package com.example.demo.Repository;
-import java.util.List;
+// package com.example.demo.Repository;
+// import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
 
-import com.example.demo.Entities.Flower;
-public interface FlowerRepository extends CrudRepository<Flower, Long> {
-    List<Flower> findAll();
-    Flower save(Flower entity);
+// import com.example.demo.Entities.Flower;
+// public interface FlowerRepository extends CrudRepository<Flower, Long> {
+//     List<Flower> findAll();
+//     Flower save(Flower entity);
 
-}
+// }
